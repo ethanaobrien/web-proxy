f584bef24bc0df261b05b6a6915a61596be8e0ca
ethanaobrien <ethan.a.obrien@gmail.com>
2022-04-14 17:12:34 +0000

    more work

--- parseText.js
+++ parseText.js
@@ -23,7 +23,7 @@ module.exports = function(body, isHtml, isUrlEncoded, opts, url, reqHost, proxyJ
         .replaceNC('"'+site2Proxy.replaceNC('\\/', '/'), '"')
         .replaceNC("'"+hostname, "'"+reqHost)
         .replaceNC('"'+hostname, '"'+reqHost)
-        .replaceNC(hostname.replaceNC('.', '\\\\.'), reqHost.replaceNC('.', '\\\\.'))
+        .replaceNC(hostname.replaceNC('www.', '').replaceNC('.', '\\\\.'), reqHost.replaceNC('.', '\\\\.'))
         .replaceNC("'"+hn2, "'"+reqHost)
         .replaceNC('"'+hn2, '"'+reqHost)
         .replaceNC('discord', 'discordddd')
