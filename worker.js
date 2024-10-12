29f40bd3975831a0c241f81136421ac833bdce66
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2024-10-12 14:02:01 -0500

    we probably don't need this

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install", (event) => {
     self.skipWaiting();
 });
-
+/*
 self.addEventListener("activate", (event) => {
     self.clients.claim();
 });
@@ -56,4 +56,6 @@ self.addEventListener("fetch", (event) => {
         })()
     );
 });
+
+*/
 
